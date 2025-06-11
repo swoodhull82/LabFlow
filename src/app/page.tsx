9c5cf6fda0ce105c8d844617d3d99335517bd996
Spencer Woodhull <wood0477@gmail.com>
2025-06-11 20:07:41 +0000

    Compiled successfully in 14.0s    Skipping validation of types    Skippi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { LoginForm } from "@/components/auth/LoginForm";
+import LoginForm from "@/components/auth/LoginForm";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
