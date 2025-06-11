9e4ca592f92cc7bb2533b12d07b30f309effa51e
Spencer Woodhull <wood0477@gmail.com>
2025-06-11 20:27:41 +0000

    https://swoodhull82.github.io/LabFlow/

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type {NextConfig} from 'next';
 
+const repoName = 'LabFlow'; // Set your repository name here
+
 const nextConfig: NextConfig = {
   output: 'export', // Add this line for static export
+  basePath: `/${repoName}`, 
+  assetPrefix: `/${repoName}/`,
   typescript: {
     ignoreBuildErrors: true,
   },
