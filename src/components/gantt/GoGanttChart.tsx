e2a17279b99538b60802a8e5fb03422fd31e1697
Spencer Woodhull <wood0477@gmail.com>
2025-07-10 17:59:07 +0000

    for validations 2.0 gantt chart: { "class": "GraphLinksModel",   "modelD

--- src/components/gantt/GoGanttChart.tsx
+++ src/components/gantt/GoGanttChart.tsx
@@ -478,27 +478,56 @@ const GoGanttChart = () => {
         // Shared Model
         const myModel = new go.GraphLinksModel({
             linkKeyProperty: 'key',
-            modelData: { origin: 1531540800000 },
-            nodeDataArray: [
-                { key: 0, text: 'Project X' },
-                { key: 1, text: 'Task 1', color: 'darkgreen' },
-                { key: 11, text: 'Task 1.1', color: 'green', duration: convertDaysToUnits(7) },
-                { key: 12, text: 'Task 1.2', color: 'green' },
-                { key: 121, text: 'Task 1.2.1', color: 'lightgreen', duration: convertDaysToUnits(3) },
-                { key: 122, text: 'Task 1.2.2', color: 'lightgreen', duration: convertDaysToUnits(5) },
-                { key: 123, text: 'Task 1.2.3', color: 'lightgreen', duration: convertDaysToUnits(4) },
-                { key: 2, text: 'Task 2', color: 'darkblue' },
-                { key: 21, text: 'Task 2.1', color: 'blue', duration: convertDaysToUnits(15), start: convertDaysToUnits(10) },
-                { key: 22, text: 'Task 2.2', color: 'goldenrod' },
-                { key: 221, text: 'Task 2.2.1', color: 'yellow', duration: convertDaysToUnits(8) },
-                { key: 222, text: 'Task 2.2.2', color: 'yellow', duration: convertDaysToUnits(6) },
-                { key: 23, text: 'Task 2.3', color: 'darkorange' },
-                { key: 231, text: 'Task 2.3.1', color: 'orange', duration: convertDaysToUnits(11) }
+            modelData: {"origin":1531540800000},
+              "nodeDataArray": [
+            {"key":0,"text":"Project X","start":0,"duration":52},
+            {"key":1,"text":"Task 1","color":"darkgreen","start":0,"duration":19},
+            {"key":11,"text":"Task 1.1","color":"green","duration":7,"start":0},
+            {"key":12,"text":"Task 1.2","color":"green","start":7,"duration":12},
+            {"key":121,"text":"Task 1.2.1","color":"lightgreen","duration":3,"start":7},
+            {"key":122,"text":"Task 1.2.2","color":"lightgreen","duration":5,"start":10},
+            {"key":123,"text":"Task 1.2.3","color":"lightgreen","duration":4,"start":15},
+            {"key":2,"text":"Task 2","color":"darkblue","start":10,"duration":29},
+            {"key":21,"text":"Task 2.1","color":"blue","duration":15,"start":10},
+            {"key":22,"text":"Task 2.2","color":"goldenrod","start":25,"duration":14},
+            {"key":221,"text":"Task 2.2.1","color":"yellow","duration":8,"start":25},
+            {"key":222,"text":"Task 2.2.2","color":"yellow","duration":6,"start":33},
+            {"key":23,"text":"Task 2.3","color":"darkorange","start":20,"duration":11},
+            {"key":231,"text":"Task 2.3.1","color":"orange","duration":11,"start":20},
+            {"key":3,"text":"Task 3","color":"maroon","start":18,"duration":34},
+            {"key":31,"text":"Task 3.1","color":"brown","duration":10,"start":42},
+            {"key":32,"text":"Task 3.2","color":"brown","start":18,"duration":25},
+            {"key":321,"text":"Task 3.2.1","color":"lightsalmon","duration":8,"start":18},
+            {"key":322,"text":"Task 3.2.2","color":"lightsalmon","duration":3,"start":26},
+            {"key":323,"text":"Task 3.2.3","color":"lightsalmon","duration":7,"start":29},
+            {"key":324,"text":"Task 3.2.4","color":"lightsalmon","duration":5,"start":29},
+            {"key":325,"text":"Task 3.2.5","color":"lightsalmon","duration":4,"start":34},
+            {"key":326,"text":"Task 3.2.6","color":"lightsalmon","duration":5,"start":38}
             ],
-            linkDataArray: [
-                { key: '0_1', from: 0, to: 1 }, { key: '1_11', from: 1, to: 11 }, { key: '1_12', from: 1, to: 12 }, { key: '12_121', from: 12, to: 121 }, { key: '12_122', from: 12, to: 122 }, { key: '12_123', from: 12, to: 123 },
-                { key: '0_2', from: 0, to: 2 }, { key: '2_21', from: 2, to: 21 }, { key: '2_22', from: 2, to: 22 }, { key: '22_221', from: 22, to: 221 }, { key: '22_222', from: 22, to: 222 }, { key: '2_23', from: 2, to: 23 }, { key: '23_231', from: 23, to: 231 },
-                { key: '11_2', from: 11, to: 2, category: 'Dep' }
+              "linkDataArray": [
+            {"key": "0_1", "from":0,"to":1},
+            {"key": "1_11", "from":1,"to":11},
+            {"key": "1_12", "from":1,"to":12},
+            {"key": "12_121", "from":12,"to":121},
+            {"key": "12_122", "from":12,"to":122},
+            {"key": "12_123", "from":12,"to":123},
+            {"key": "0_2", "from":0,"to":2},
+            {"key": "2_21", "from":2,"to":21},
+            {"key": "2_22", "from":2,"to":22},
+            {"key": "22_221", "from":22,"to":221},
+            {"key": "22_222", "from":22,"to":222},
+            {"key": "2_23", "from":2,"to":23},
+            {"key": "23_231", "from":23,"to":231},
+            {"key": "0_3", "from":0,"to":3},
+            {"key": "3_31", "from":3,"to":31},
+            {"key": "3_32", "from":3,"to":32},
+            {"key": "32_321", "from":32,"to":321},
+            {"key": "32_322", "from":32,"to":322},
+            {"key": "32_323", "from":32,"to":323},
+            {"key": "32_324", "from":32,"to":324},
+            {"key": "32_325", "from":32,"to":325},
+            {"key": "32_326", "from":32,"to":326},
+            {"key": "11_2", "from":11,"to":2,"category":"Dep"}
             ]
         });
 
@@ -603,7 +632,3 @@ const GoGanttChart = () => {
 };
 
 export default GoGanttChart;
-
-    
-
-    
